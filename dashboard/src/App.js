d28ea9c83f298025512564f4ea91575b2ed0cb1e
Dre-OS <hlsagabaen@gmail.com>
2023-10-24 10:30:06 +0800

    created login page

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -5,11 +5,13 @@ function App() {
   return (
     <div className="App">
       <h2>Login Form</h2>
-      <div>
+      <div id="login">
+        Username: &nbsp;<br />
         <input type="text" id="username" />
-        <br />
+        <br /><br />
+        Password: &nbsp;<br />
         <input type="password" id="password" />
-        <br />
+        <br /><br />
         <button type="submit">Login</button>
       </div>
     </div>
